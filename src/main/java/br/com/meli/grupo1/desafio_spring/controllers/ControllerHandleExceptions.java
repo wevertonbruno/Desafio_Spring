517a02f037c23e4da2bcf1fa190062fa0801b6d9
Weverton Bruno <weverton.ssantos@mercadolivre.com>
2022-03-23 16:01:28 -0300

    feat: Criando estrutura de pastas

---
+++ src/main/java/br/com/meli/grupo1/desafio_spring/controllers/ControllerHandleExceptions.java
@@ -0,0 +1,7 @@
+package br.com.meli.grupo1.desafio_spring.controllers;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ControllerHandleExceptions {
+}
