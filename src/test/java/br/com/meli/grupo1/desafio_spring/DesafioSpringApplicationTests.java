08605f2e6eac86f669c8c66526b539c74d9ea3b0
Weverton Bruno <weverton.ssantos@mercadolivre.com>
2022-03-23 11:31:26 -0300

    feat: inicializando projeto

---
+++ src/test/java/br/com/meli/grupo1/desafio_spring/DesafioSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meli.grupo1.desafio_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
