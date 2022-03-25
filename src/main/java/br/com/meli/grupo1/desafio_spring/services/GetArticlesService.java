0511859c9363f0e40ec1ad28672737433467fc57
Jefferson <jefferson.botelho@mercadolivre.com>
2022-03-25 11:48:07 -0300

    bug: corrigido parametro de busca por nome

--- src/main/java/br/com/meli/grupo1/desafio_spring/services/GetArticlesService.java
+++ src/main/java/br/com/meli/grupo1/desafio_spring/services/GetArticlesService.java
@@ -33,31 +33,41 @@ public class GetArticlesService {
     }
 
     public ArticlesDTO findArticles (String category, Boolean freeShipping, String productName, String brandName, Integer typeOrder) {
+
 //        List<Product> productsList = products.getAll();
-        List<Product>productsList = new ArrayList<>();
+        List<Product> productsList = new ArrayList<>();
         productsList = productsListRepo;
+
         if (category != null && productsList != null)
             productsList = productsList
                     .stream()
                     .filter(article -> article.getCategory().equalsIgnoreCase(category))
                     .collect(Collectors.toList());
+
+
         if (freeShipping != null && productsList != null)
             productsList = productsList
                     .stream()
                     .filter(article -> article.getFreeShipping() && freeShipping)
                     .collect(Collectors.toList());
+
+
         if (productName != null && productsList != null)
             productsList = productsList
                     .stream()
-                    .filter(article -> article.getBrand().equalsIgnoreCase(productName))
+                    .filter(article -> article.getName().equalsIgnoreCase(productName))
                     .collect(Collectors.toList());
+
+
         if (brandName != null && productsList != null)
             productsList = productsList
                     .stream()
                     .filter(article -> article.getBrand().equalsIgnoreCase(brandName))
                     .collect(Collectors.toList());
+
+
         if (typeOrder != null && productsList != null)
-            switch (typeOrder){
+            switch (typeOrder) {
                 case 0:
                     productsList = productsList
                             .stream()
@@ -83,12 +93,16 @@ public class GetArticlesService {
                             .collect(Collectors.toList());
                     break;
             }
+            try {
+                return ArticlesDTO.convertToDTO(productsList);
+            } catch (NullPointerException e) {
+                //tratar desta exception
+            }
 
-        try {
-            return ArticlesDTO.convertToDTO(productsList);
-        } catch (NullPointerException e) {
-            //tratar desta exception
-        }
+
+
         return new ArticlesDTO();
+
+
     }
 }
