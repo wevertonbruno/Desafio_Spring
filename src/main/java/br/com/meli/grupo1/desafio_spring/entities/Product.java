97dc5b745f7a28c52c2b59190e202b86f29994d3
Jefferson <jefferson.botelho@mercadolivre.com>
2022-03-25 11:47:05 -0300

    refactor: substituido construtor, setter e getters por notacoes do lombok para deixar o codigo mais limpo

--- src/main/java/br/com/meli/grupo1/desafio_spring/entities/Product.java
+++ src/main/java/br/com/meli/grupo1/desafio_spring/entities/Product.java
@@ -1,8 +1,15 @@
 package br.com.meli.grupo1.desafio_spring.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 import java.math.BigDecimal;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product implements Serializable {
     private Long productId;
     private String name;
@@ -13,81 +20,4 @@ public class Product implements Serializable {
     private Boolean freeShipping;
     private String prestige;
 
-    public Product(){}
-
-    public Product(Long productId, String name, String category, String brand, BigDecimal price, Integer quantity,
-                   Boolean freeShipping, String prestige) {
-        this.productId = productId;
-        this.name = name;
-        this.category = category;
-        this.brand = brand;
-        this.price = price;
-        this.quantity = quantity;
-        this.freeShipping = freeShipping;
-        this.prestige = prestige;
-    }
-
-    public Long getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Long productId) {
-        this.productId = productId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getBrand() {
-        return brand;
-    }
-
-    public void setBrand(String brand) {
-        this.brand = brand;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public Integer getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(Integer quantity) {
-        this.quantity = quantity;
-    }
-
-    public Boolean getFreeShipping() {
-        return freeShipping;
-    }
-
-    public void setFreeShipping(Boolean freeShipping) {
-        this.freeShipping = freeShipping;
-    }
-
-    public String getPrestige() {
-        return prestige;
-    }
-
-    public void setPrestige(String prestige) {
-        this.prestige = prestige;
-    }
 }
